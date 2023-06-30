f0a28656e13c0ddd6b112f4f6aaba402009152ae
mariaarnesson <maria_arnesson@outlook.com>
2023-06-30 06:04:45 +0000

    Added home page

--- reservation/views.py
+++ reservation/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.views import View
 
 
 def home(request):
-    return render(request, "home")
+    return render(request, "home.html", {})
+
+
--- restaurant/settings.py
+++ restaurant/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.sites',
     'django.contrib.messages',
     'cloudinary_storage',
     'django.contrib.staticfiles',
