ec16ed829c595859f83d74d998460ee9ada1effb
mariaarnesson <maria_arnesson@outlook.com>
2023-06-30 06:40:03 +0000

    added allauth templates and defined that in settings.py

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -46,8 +46,17 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'reservation',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
