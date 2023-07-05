6a7d798f6cf7b2a85c148dbfd46a835217a3c311
mariaarnesson <maria_arnesson@outlook.com>
2023-07-05 19:47:14 +0000

    Installed crispy forms

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -52,6 +52,8 @@ INSTALLED_APPS = [
     'reservation',
     'menu',
     'home',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 SITE_ID = 1
@@ -59,6 +61,9 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
