be1e25045517020bd23284f09fcf4f51f87c78de
mariaarnesson <maria_arnesson@outlook.com>
2023-07-07 09:14:36 +0000

    Added objects menu to views and tempalte menu.html

--- menu/views.py
+++ menu/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Menu
 
 
 def menu(request):
-    return render(request, 'menu.html')
+    menus = Menu.objects.all()
+    return render(request, 'menu.html', {'menus': menus})
