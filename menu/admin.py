57e358e4bf4a0b1368250eb67a31b6c46b128948
mariaarnesson <maria_arnesson@outlook.com>
2023-07-07 08:09:02 +0000

    Added model menu

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Menu
 
-# Register your models here.
+
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('title', 'mealName', 'price')
+    search_fields = ('title', 'mealName')
+
+
+admin.site.register(Menu, MenuAdmin)
--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Menu(models.Model):
+    featured_image = CloudinaryField('image', default='placeholder')
+    title = models.CharField(max_length=100)
+    mealName = models.CharField(max_length=100)
+    mealDescription = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.title
