a1a26c46b4d1bf66c8f9600cfab72dac60ed7419
mariaarnesson <maria_arnesson@outlook.com>
2023-07-08 12:34:14 +0000

    Added meal.html, defined object meal in view

--- menu/urls.py
+++ menu/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('menu', views.menu_view, name='menu'),
+    path('meal/<int:menu_id>/', views.meal_view, name='meal'),
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Menu, Meal, Drink
 
 
@@ -6,3 +6,9 @@ def menu_view(request):
     menus = Menu.objects.all()
     print(len(menus))
     return render(request, 'menu.html', {'menus': menus})
+
+
+def meal_view(request, menu_id):
+    menu = get_object_or_404(Menu, pk=menu_id)
+    meals = menu.meal_set.all()
+    return render(request, 'meal.html', {'meals': meals})
