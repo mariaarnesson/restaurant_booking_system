7e1b023f2c8b66182c37ba6fb9b4b32e26158c19
mariaarnesson <maria_arnesson@outlook.com>
2023-07-10 22:55:05 +0000

    Added booking form and date form

---
+++ booking_system/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+class BookingForm(forms.Form):
+    name = forms.CharField(max_length=100, label='Your Name')
+    email = forms.EmailField(label='Your Email')
+
+class DateForm(forms.Form):
+    date = forms.DateField(
+        label='Date',
+        widget=forms.DateInput(attrs={'class': 'datepicker'})
+    )    
