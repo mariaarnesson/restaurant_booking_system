c61fb7b5f7b1b811c3f59e713bf87a28c8fbfccb
mariaarnesson <maria_arnesson@outlook.com>
2023-07-10 23:36:44 +0000

    Added choose date view

--- booking_system/urls.py
+++ booking_system/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.book_a_table, name='book_a_table'),
+    path('choose_date/', views.ChooseDateView.as_view(), name='choose_date'),
 ]
--- booking_system/views.py
+++ booking_system/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import generic, View
+from .forms import BookingForm, DateForm
+from .models import AvailableTime, Reservation
+from django.views import View
 
 
 def book_a_table(request):
     return render(request, "book_a_table.html", {})
+
+
+class ChooseDateView(View):
+    def get(self, request):
+        date_form = DateForm()
+        return render(request, 'choose_date.html', {'date_form': date_form})    
